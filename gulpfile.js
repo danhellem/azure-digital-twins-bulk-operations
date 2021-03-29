7f39e94bb1c6951e13dbb3839dc07d6128cd56d8
Dan Hellem <dan_hellem@hotmail.com>
2021-03-29 07:49:23 -0700

    json to ndjson work

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,34 @@
-const gulp = require('gulp')
-const del = require('del');
-const concat = require('gulp-concat');
-const ndjson = require('ndjson')
+const gulp = require("gulp");
+const del = require("del");
+const concat = require("gulp-concat");
+const fs = require("fs");
 
-gulp.task('js', function(done) { 
-  gulp.src('models/**.json')
-    .pipe(concat('single.json', {newLine: ',\r\n'} ))
-    .pipe(gulp.dest('dist'));
+gulp.task("js", function (done) {
+  gulp
+    .src("models/**.json")
+    .pipe(concat("single.json", { newLine: ",\r\n" }))
+    .pipe(gulp.dest("dist"));
+
+   
   done();
 });
 
-gulp.task('clean', function (done) {
-  del(['dist/**/*']);
+gulp.task("clean", function (done) {
+  del(["dist/**/*"]);
   done();
+});
+
+gulp.task("ndjson", function (done) {
+  const arrObj = JSON.parse(fs.readFileSync('dist/test.json'));
+  const ndJson = arrObj.map(JSON.stringify).join('\n');
+  console.log(ndJson);
+  done();
+
 });
 
 function defaultTask(cb) {
-    // place code for your default task here
-    cb();
-  }
-  
-  exports.default = defaultTask
+  // place code for your default task here
+  cb();
+}
+
+exports.default = defaultTask;
