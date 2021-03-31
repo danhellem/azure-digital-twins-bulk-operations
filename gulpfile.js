26953cddf3ae47ff1a3d205203df6c1babaf770a
Dan Hellem <dan_hellem@hotmail.com>
2021-03-31 09:52:58 -0700

    getting poc working

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,56 @@
-const gulp = require("gulp");
-const del = require("del");
-const concat = require("gulp-concat");
-const fs = require("fs");
+const gulp = require('gulp');
+const del = require('del');
+const concat = require('gulp-concat');
+const gap = require('gulp-append-prepend');
+const fs = require('fs');
+const log = require('fancy-log');
 
-gulp.task("js", function (done) {
+gulp.task('create-file', function (done) {
+  log('Creating dist/staging.json from all models in models directory');
+
   gulp
-    .src("models/**.json")
-    .pipe(concat("single.json", { newLine: ",\r\n" }))
-    .pipe(gulp.dest("dist"));
-
-   
+    .src('models/**.json')
+    .pipe(concat('staging.json', { newLine: ',\r\n' }))
+    .pipe(gulp.dest('dist'));
   done();
 });
 
-gulp.task("clean", function (done) {
-  del(["dist/**/*"]);
+gulp.task('add-header', function (done) {
+  log('Prepending header.json text and Models section dist/staging.json');
+
+  gulp
+    .src('dist/staging.json')
+    .pipe(gap.prependText('{"Section": "Models"}', ',\r\n'))
+    .pipe(gap.prependFile('header.json', ',\r\n'))
+    .pipe(gap.prependText('[', '\r\n'))
+    .pipe(gap.appendText(']'))
+    .pipe(concat('staging.json'))
+    .pipe(gulp.dest('dist'));
   done();
 });
 
-gulp.task("ndjson", function (done) {
-  const arrObj = JSON.parse(fs.readFileSync('dist/test.json'));
+gulp.task('clean', function (done) {
+  log('Deleting all files from dist directory');
+
+  del(['dist/**/*']);
+  done();
+});
+
+gulp.task('ndjson', function (done) {
+  log('Converting dist/staging.json into ndjson formatted file');
+
+  const arrObj = JSON.parse(fs.readFileSync('dist/staging.json'));
   const ndJson = arrObj.map(JSON.stringify).join('\n');
-  console.log(ndJson);
+  
+  fs.writeFile('dist/import.ndjson', ndJson, done);  
+  
   done();
-
 });
+
 
 function defaultTask(cb) {
-  // place code for your default task here
+ 
+  
   cb();
 }
 
