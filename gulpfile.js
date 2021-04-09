3eb329b3c15d88edb1392d7deb7fa10529e5e2a1
Dan Hellem <dan_hellem@hotmail.com>
2021-04-09 13:50:14 -0700

    clean up

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,22 @@
 const gulp = require('gulp');
+const series = require('gulp');
 const del = require('del');
 const concat = require('gulp-concat');
 const gap = require('gulp-append-prepend');
 const fs = require('fs');
 const log = require('fancy-log');
 
-gulp.task('create-staging', function (done) {
+gulp.task('create-staging', (cb) => {
   log('Creating dist/staging.json from all models in models directory');
 
   gulp
     .src('models/**.json')
     .pipe(concat('staging.json', { newLine: ',\r\n' }))
     .pipe(gulp.dest('dist'));
-  done();
+  cb();
 });
 
-gulp.task('add-header', function (done) {
+gulp.task('add-header', (cb) => {
   log('Prepending header.json text and Models section dist/staging.json');
 
   gulp
@@ -27,27 +28,26 @@ gulp.task('add-header', function (done) {
     .pipe(gap.appendText(']'))
     .pipe(concat('staging.json'))
     .pipe(gulp.dest('dist'));
-  done();
+  cb();
 });
 
-gulp.task('clean', function (done) {
+gulp.task('clean', (cb) => {
   log('Deleting all files from dist directory');
 
   del(['dist/**/*']);
-  done();
+  cb();
 });
 
-gulp.task('create-ndjson', function (done) {
+gulp.task('create-ndjson', (cb) => {
   log('Converting dist/staging.json into ndjson formatted file');
 
   const arrObj = JSON.parse(fs.readFileSync('dist/staging.json'));
   const ndJson = arrObj.map(JSON.stringify).join('\n');
   
-  fs.writeFile('dist/import.ndjson', ndJson, done);  
+  fs.writeFile('dist/import.ndjson', ndJson, cb);  
   
-  done();
+  cb();
 });
-
 
 function defaultTask(cb) {  
   cb();
